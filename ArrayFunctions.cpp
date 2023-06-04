83faa3cf9bfa7d012853bb853a67be8a72a888ec
Shreya Kesarwani <93576246+ShreyaKesarwani1922@users.noreply.github.com>
2023-06-04 18:09:35 +0530

    Create ArrayFunctions.cpp

---
+++ ArrayFunctions.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+// function for taking input in an array
+void inputArray(int a[], int size){
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
+    }
+    
+}
+
+// function for printing elements of an array
+void printArray(int a[], int size){
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+// function for getting sum of elements of an array
+int sumArray(int a[], int size){
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = sum + a[i];
+    }
+    
+}
+int main()
+{
+    
+    int n;      // n = size of the array
+    cin >> n;   // taking input for n
+    int a[n];   // initialising array a[]
+
+
+
+}
